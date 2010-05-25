2062cb1bec70f725a53200a5c46e7e9b6004479a
lono175 <khaifeng@gmail.com>
2010-05-24 20:36:28 -0700

    Add SARSA controller

--- Environment.py
+++ Environment.py
@@ -1,4 +1,5 @@
 import sys,pygame
+from SARSA import SARSA
 class Environment:
     def __init__(self, size):
         self.size = self.width, self.height = size
@@ -27,24 +28,25 @@ class Environment:
         return reward, state, flag
 
     def getReward(self):
-        if self.ballRect.top > self.height:
-            return -1
+        #if self.ballRect.top > self.height:
+        if self.ballRect.bottom > self.height:
+            return -1000
         else:
-            return 0
+            return 1
 
     def isTerminal(self):
-        if self.getReward() == -1:
+        if self.getReward() == -1000:
             return True
         if self.stepNum > 5000:
             return True
         return False
 
     def getState(self):
-        return (self.ballRect.center[0], self.ballRect.center[1], self.barRect.center[0])
+        return (self.ballRect.center[0]/10, self.ballRect.center[1]/10, self.barRect.center[0]/10)
 
     def updateState(self, action):
         if self.ballRect.colliderect(self.barRect):
-            self.speed[1] = -self.speed[1]
+            self.speed[1] = -abs(self.speed[1])
         if self.ballRect.left < 0 or self.ballRect.right > self.width:
             self.speed[0] = -self.speed[0]
         if self.ballRect.top < 0:
@@ -57,6 +59,12 @@ class Environment:
             self.barRect = self.barRect.move(self.barSpeed) 
         if action == -1:
             self.barRect = self.barRect.move([-self.barSpeed[0], -self.barSpeed[1]]) 
+
+        #check the bar stays in the boundary
+        if self.barRect.right > self.width:
+            self.barRect.right = self.width
+        if self.barRect.left < 0:
+            self.barRect.left = 0
 
 
 
@@ -71,28 +79,36 @@ class Environment:
 size = 200, 200
 delay = 300
 interval = 50
+action = 0
 pygame.init()
 pygame.key.set_repeat(delay, interval)
-screen = pygame.display.set_mode(size)
-env = Environment(size)
-env.start()
 clock=pygame.time.Clock()
+screen = pygame.display.set_mode(size)
+
+
+agent = SARSA(0.1, 0.1, 0.9, (-1, 1))
+
 while 1:
-    action = 0
-    clock.tick(30)
-    for event in pygame.event.get():
-       if event.type == pygame.QUIT: sys.exit()
-       if event.type==pygame.KEYDOWN:
-            if event.key==pygame.K_LEFT:
-                action = -1
-            if event.key==pygame.K_RIGHT:
-                action = 1
-    (reward, state, isTerminal) = env.step(action)
-    screen.blit(env.getScreen(), (0, 0))
-    print reward
-    print state
-    print isTerminal
-    pygame.display.flip()
-    if isTerminal:
-        break
+    env = Environment(size)
+    state = env.start()
+    action = agent.start(state)
+    while 1:
+        clock.tick(1000)
+        for event in pygame.event.get():
+           if event.type == pygame.QUIT: sys.exit()
+           #if event.type==pygame.KEYDOWN:
+                #if event.key==pygame.K_LEFT:
+                    #action = -1
+                #if event.key==pygame.K_RIGHT:
+                    #action = 1
+        (reward, state, isTerminal) = env.step(action)
+        action = agent.step(reward, state)
+        screen.blit(env.getScreen(), (0, 0))
+        print reward
+        print state
+        print isTerminal
+        pygame.display.flip()
+        if isTerminal:
+            agent.end(reward)
+            break
 
